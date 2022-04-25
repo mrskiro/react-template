8a5a7a58681ea02d791a212d830c03ee4bc325f4
purpleeeee <h.purpleeeee@gmail.com>
2022-04-25 17:47:02 +0900

    feat eslint and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { App } from "./App"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root")
+if (!rootElement) throw new Error("Failed to find the root element")
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
