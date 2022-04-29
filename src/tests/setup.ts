d6d663474b4b30064e7ac6a7a356896c317733ff
purpleeeee <h.purpleeeee@gmail.com>
2022-04-29 22:58:21 +0900

    chore

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom"
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#configuring-your-testing-environment
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 ;(globalThis as any).IS_REACT_ACT_ENVIRONMENT = true
